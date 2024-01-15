f1d5c689faa7c6f71e0d229718c3e6c4b009dbca
ShamanBlackout <47762410+ShamanBlackout@users.noreply.github.com>
2024-01-15 13:31:05 -0600

    Update AddressChaser.ts

--- PhantasmaTs/AddressChaser.ts
+++ PhantasmaTs/AddressChaser.ts
@@ -7,6 +7,30 @@ import { PhantasmaTS } from "phantasma-ts";
 const host = "http://localhost:7077/rpc";
 const nexus = "simnet";
 
+//ts-ignore to ignore typescript warning about API null
+//@ts-ignore
+const RPC = new PhantasmaTS.PhantasmaAPI(host, null, nexus);
+//collect addresses
+async function collectAddresses(
+  account: string,
+  page: number,
+  pageSize: number
+) {
+  //return a collection of addresses --in what format
+  let results = RPC.getAddressTransactions(account, page, pageSize);
+  results.then((value) => {
+    //console.log(value);
+    for (let x of value["result"]["txs"]) {
+      console.log(x);
+    }
+  });
+}
+
+//Filter collection based user input
+async function filterCollection() {
+  //return the filtered collection of addresses
+}
+
 /*****************************************************************
  *
  * An address should be inputted and "chased"
@@ -22,10 +46,45 @@ const nexus = "simnet";
  * @chain - chain where the address should be chased
  ****************************************************************/
 async function chaseAddress(account: string, level: number, chain: string) {
-  //ts-ignore to ignore typescript warning about API null
-  //@ts-ignore
-  const RPC = new PhantasmaTS.PhantasmaAPI(host, null, nexus);
+  let addressCount = RPC.getAddressTransactionCount(account, chain);
+
+  //Addresses ignore
+
+  /***************************************************************
+   * Loop
+   * 1) Get an amount of transactions within an address
+   * 2) Page through transactions and filter out un-needed transactions
+   * 3) Go through filtered transactions according to level of deepness
+  
+   ***************************************************************/
 }
 
-//P2K9zmyFDNGN6n6hHiTUAz6jqn29s5G1SWLiXwCVQcpHcQb - first address
-//P2K65RZhfxZhQcXKGgSPZL6c6hkygXipNxdeuW5FU531Bqc -2nd addresss
+//Questions: How to do page and Pagesize factor in
+
+let results = collectAddresses(
+  "P2K9zmyFDNGN6n6hHiTUAz6jqn29s5G1SWLiXwCVQcpHcQb",
+  1,
+  25
+);
+
+/****************************************************************************************
+ * hash: string; //Hash of the transaction
+ * chainAddress: string; //Transaction chain address
+ *  timestamp: number; //Block time
+ *  blockHeight: number; //Block height at which the transaction was accepted
+ *  blockHash: string; //Hash of the block
+ *  script: string; //Script content of the transaction, in hexadecimal format
+ *  payload: string; //Payload content of the transaction, in hexadecimal format
+ *  events: Array<Event>; //List of events that triggered in the transaction
+ *  result: string; //Result of the transaction, if any. Serialized, in hexadecimal format
+ *  fee: string; //Fee of the transaction, in KCAL, fixed point
+ *  state: string;
+ *  signatures: Array<SignatureResult>;
+ *  sender: string;
+ *  gasPayer: string;
+ *  gasTarget: string;
+ *  gasPrice: string;
+ *  gasLimit: string;
+ *  expiration: number;
+ *
+ ******************************************************************************************/
